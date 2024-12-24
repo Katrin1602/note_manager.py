7ec94de5006b7070348d6ad174fe3876f4c56e23
Katrin1602 <bulycheva.katrin@gmail.com>
2024-12-24 17:50:19 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,19 @@
+username = 'Екатерина'
+print("имя пользователя:", username)
+
+title='Заголовок заметки'
+print(title)
+
+content='Описание заметки'
+print(content)
+
+status="Статус заметки"
+print(status)
+
+created_date="24.12.2024"
+print(created_date)
+
+issue_date="31.12.2024"
+print(issue_date)
+
+
