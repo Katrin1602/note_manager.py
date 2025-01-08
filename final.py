4e2b22cc90d78c5b6ca855ee6923dfcfbab9811e
Katrin1602 <bulycheva.katrin@gmail.com>
2025-01-08 20:31:50 +0300

    Add files via upload

---
+++ final.py
@@ -0,0 +1,42 @@
+note = ["username" ,
+
+"title",
+
+["title 1", "title 2", "title 3"],
+
+"content",
+
+"status",
+
+"created_date",
+
+"issue_date"
+        ]
+
+
+print(note)
+
+username = input( 'Введите Ваше имя: ')
+print( username )
+
+title = input( 'Введите Заголовок заметки: ')
+print(title)
+
+title1 = input('введите подзаголовок 1: ')
+title2 = input('введите подзаголовок 2: ')
+title3 = input('введите подзаголовок 3: ')
+print(title1, title2, title3)
+
+content = input( 'Напишите описание заметки: ')
+print(content)
+
+status = input( 'Укажите статус заметки: ')
+print(status)
+
+created_date = input("дата создания заметки в формате день-месяц-год: ")
+print(created_date)
+
+issue_date = input("дата истечения заметки в формате день-месяц-год: ")
+print(issue_date)
+
+# это задание не поняла как делать. Уверена, что непраильно сделала
