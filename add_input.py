b65673bc4bed440b180cf856b9644116fd5e96e5
Katrin1602 <bulycheva.katrin@gmail.com>
2025-01-08 18:53:08 +0300

    Add files via upload

---
+++ add_input.py
@@ -0,0 +1,17 @@
+username = input( 'Введите Ваше имя: ')
+print( username )
+
+title = input( 'Введите Заголовок заметки: ')
+print(title)
+
+content = input( 'Напишите описание заметки: ')
+print(content)
+
+status = input( 'Укажите статус заметки: ')
+print(status)
+
+created_date = input("дата создания заметки в формате день-месяц-год: ")
+print(created_date)
+
+issue_date = input("дата истечения заметки в формате день-месяц-год: ")
+print(issue_date)
